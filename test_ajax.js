89cb9c6ba3875009e91fa60500eee675f7f85496
simoacqua <simoacqua@gmail.com>
2017-08-08 15:45:11 +0200

    test con git 1

---
+++ test_ajax.js
@@ -0,0 +1,35 @@
+$('.classe_padre').on('click','button',function(){
+	$.ajax('url_relativo.html', {
+		success: function(response){
+			$('.classe_div_da_visualizzare').html(response).slideDown();
+		},
+		data: { 'key': 'value' }
+	})
+
+});
+
+// or
+
+$('.classe_padre').on('click','button',function(){
+	$.get('url_relativo.html', function(response){
+		$('.classe_div_da_visualizzare').html(response).slideDown();
+	});
+});
+
+// or
+
+
+// <div class="classe_div" data-info="valore"></div>
+
+$('.classe_padre').on('click','button',function(){
+
+	$.ajax('url_relativo.html', {
+		success: function(response){
+			$('.classe_div').html(response).slideDown(); // carico nel div l'html ricevuto dal server
+		},
+		data: { "info" : $('.classe_div').data('info'): }, // ricevo i parametri impostati nell'html tramite l'elemento data
+	)};
+
+});
+
+
